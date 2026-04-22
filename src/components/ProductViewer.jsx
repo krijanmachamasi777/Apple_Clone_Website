a2867af5201b1b02252bae9711d2c29778dd6ac1
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2026-04-22 09:12:42 +0545

    modelswitcher added

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,24 +1,55 @@
 import React from 'react'
+import clsx from 'clsx'
+import useMacbookStore from '../store'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Box } from '@react-three/drei'
+import MacbookModel14 from '../constants/Models/Macbook-14'
+import StudioLights from './three/StudioLights'
 
+import ModelSwitcher from './three/ModelSwitcher.jsx'
+import { useMediaQuery } from 'react-responsive'
 const ProductViewer = () => {
+
+    const { color, scale, setColor, setScale } = useMacbookStore();
+
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)' });
     return (
         <section id="product-viewer">
             <h2>Take a closer look .</h2>
 
             <div className="controls">
-                <p className="info">MacbookPro 16" in Space Black</p>
+                <p className="info">MacbookPro {scale} in {color}</p>
 
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
-                        <div className="bg-neutral-300" />
-                        <div className="bg-neutral-900" />
+                        <div
+                            onClick={() => setColor('#adb5bd')}
+                            className={clsx('bg-netural-300', color == '#adb5bd' && 'active')} />
+                        <div 
+                        onClick={() => setColor('#2e2c2e')}
+                         className={clsx('bg-netural-900',color=='#2e2c2e' && 'active')} />
                     </div>
                     <div className="size-control">
-                        <div><p>14"</p></div>
-                        <div><p>14"</p></div>
+                        <div 
+                        onClick={() => setScale(0.06)}
+                         className={clsx(scale==0.06? 'bg-white text-black':'bg-transparent text-white')} >
+                            <p>14"</p>
+                        </div>
+                         <div 
+                        onClick={() => setScale(0.08)}
+                         className={clsx(scale==0.08? 'bg-white text-black':'bg-transparent text-white')} >
+                            <p>16"</p>
+                        </div>
                     </div>
                 </div>
             </div>
+           <Canvas id="canvas" camere={{position:[0,2,5],fov:50,near:0.1,far:100}}>
+               <StudioLights />
+          
+
+            <ModelSwitcher scale={isMobile ? scale -0.03 :scale} isMobile={isMobile} />
+           </Canvas>
         </section>
     )
 }
