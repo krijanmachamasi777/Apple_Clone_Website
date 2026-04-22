e46afd63805d165db1688fb2e6ae2f9d74d36b36
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2026-04-22 07:36:18 +0545

    productviewer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
+import ProductViewer from "./components/ProductViewer"
 function App() {
   return (
     <main>
      <NavBar />
      <Hero />
+     <ProductViewer/>
     </main>
   )
 }
